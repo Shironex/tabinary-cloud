42bbd95262b1d78bde58c80ca04ddd559b0ba7a0
Shirone <kacperlachowiczwp.pl@wp.pl>
2023-09-28 17:15:52 +0200

    - initial repo

---
+++ src/app/(landing)/page.tsx
@@ -0,0 +1,10 @@
+import MaxWidthWrapper from '@/components/max-width-wrapper'
+import React from 'react'
+
+const LandingHomePage = () => {
+  return (
+    <MaxWidthWrapper className="mb-12 mt-28 sm:mt-40 flex flex-col items-center justify-center">LandingHomePage</MaxWidthWrapper>
+  )
+}
+
+export default LandingHomePage
